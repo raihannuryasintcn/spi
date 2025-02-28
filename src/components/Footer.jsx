cf6bf967f4f201495a3291d3ed72c35dc5cb426c
raihannuryasintcn <raihannuryasintcn@gmail.com>
2025-02-28 09:03:40 +0700

    update main and sidebar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export function Footer() {
     return (
         <footer>
-            <p></p>
+            <p><b>OFFICE/WORKSHOP</b><br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fuga, dolorem cupiditate praesentium esse, tempora minus at eligendi blanditiis, illo hic ad dignissimos suscipit laboriosam harum optio! Eaque, quibusdam officia!
+            </p>
         </footer>
     );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import './style.css'
+import { Sidebar } from '../../components/SidebarWidget';
 
 export function Home() {
     return(
@@ -10,11 +11,11 @@ export function Home() {
                     <div className="MainBanner"></div>
 
                     <div className="HeadingContainer">
-                        <h3></h3>
+                        <h3>PROFIL SPI AC SPECIALIST</h3>
                     </div>
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quos officiis blanditiis! Asperiores, architecto? Molestiae, praesentium natus quae consequatur eius placeat labore corrupti ipsam veritatis vero quibusdam voluptas excepturi quidem, ea porro cumque, dignissimos voluptates laudantium. Quas placeat consequatur, vitae voluptates molestias, dicta quod veritatis corporis quisquam rerum nihil? Aut!</p>
 
-                    <div className="Quotes"></div>
+                    <div className="Quotes">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores nihil esse omnis delectus aspernatur ex cumque velit sint dolorum nostrum.</div>
 
                     <div className="MainGridContainer">
                         <p></p>
@@ -22,13 +23,13 @@ export function Home() {
                     </div>
 
                     {/* VISI */}
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat consectetur et dolores quas hic a, ratione ipsa pariatur optio at blanditiis rerum aut labore! Atque veniam ut animi neque inventore.</p>
 
                     {/* MISI */}
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sequi iste doloribus dolor totam distinctio dignissimos, deserunt aspernatur est, fugiat voluptatum! Nemo dolor voluptas, quod animi natus aperiam repellendus ducimus.</p>
 
                     <div className="HeadingContainer">
-                        <h3></h3>
+                        <h3>OUR CLIENTS</h3>
                     </div>
                     <div className="SliderOurClients"></div>
 
@@ -41,7 +42,9 @@ export function Home() {
 
             
             <div className="Sidebar">
-                <div className="wrapper"></div>
+                <div className="wrapper">
+                    <Sidebar />
+                </div>
             </div>
         </div>
     );
