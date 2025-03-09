110b1ad50eaae1b4a9aacf78b1cf062224d605fe
raihannuryasintcn <raihannuryasintcn@gmail.com>
2025-03-09 14:45:06 +0700

    optimize sewa pages

--- src/pages/Sewa.jsx
+++ src/pages/Sewa.jsx
@@ -55,7 +55,7 @@ export function Sewa() {
                 </div>
 
                 <Heading title="SEWA ALAT"/>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-4">
+                <div className="grid grid-cols-2 gap-6 py-4">
                     {equipmentData.map((equipment) => (
                         <EquipmentCard key={equipment.id} equipment={equipment} />
                     ))}
