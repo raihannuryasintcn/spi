de0c2c21f4f27944146fc15051d9cc4c79fd601e
adriyansptze <adriyanseptiazi@gmail.com>
2025-03-10 14:15:21 +0700

    mengisi prinsip perusahaan dan teks bergerak

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -39,21 +39,48 @@ export function About() {
         <div className="grid grid-cols-12 w-full h-auto">
             <div className="bg-gray-100 w-full p-4 col-span-9">
                 <div className="text-red-500 italic">
-                    <marquee behavior="scroll" direction="left">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet delectus harum voluptatum excepturi rerum illum cum sint earum perferendis labore.</marquee>
+                    <marquee behavior="scroll" direction="left"> Kami menjunjung tinggi nilai kepercayaan, etika, moral, dan kejujuran di setiap langkah dalam melakukan kegiatan dan pengambilan keputusan.</marquee>
                 </div>
 
                 <div className="flex items-center pb-4">
                     <img src={main} alt="" className="object-fill"/>
                 </div>
                 <Heading title="PRINSIP-PRINSIP PERUSAHAAN"/>
-                <h1 className="text-gray-700 text-xl py-4 font-semibold">Lorem ipsum dolor sit amet.</h1>
-                <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe reprehenderit enim perspiciatis nam sint molestias eligendi aliquid dolor non dolores.</p>
-                <h1 className="text-gray-700 text-xl py-4 font-semibold">Lorem ipsum dolor sit amet.</h1>
-                <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe reprehenderit enim perspiciatis nam sint molestias eligendi aliquid dolor non dolores.</p>
-                <h1 className="text-gray-700 text-xl py-4 font-semibold">Lorem ipsum dolor sit amet.</h1>
-                <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe reprehenderit enim perspiciatis nam sint molestias eligendi aliquid dolor non dolores.</p>
-                <h1 className="text-gray-700 text-xl py-4 font-semibold">Lorem ipsum dolor sit amet.</h1>
-                <p className="text-gray-600 pb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe reprehenderit enim perspiciatis nam sint molestias eligendi aliquid dolor non dolores.</p>
+                <h1 className="text-gray-700 text-xl py-4 font-semibold">Disiplin</h1>
+                <p className="text-gray-600">
+                Taat dan patuh pada nilai-nilai yang menjadi tanggung jawab perusahaan.
+                </p>
+
+                <h1 className="text-gray-700 text-xl py-4 font-semibold">Teguh Pendirian</h1>
+                <p className="text-gray-600">
+                Konsisten dalam melayani pelanggan dengan sebaik-baiknya dan menjaga jati diri sebagai pekerja mesin pendingin.
+                </p>
+
+                <h1 className="text-gray-700 text-xl py-4 font-semibold">Ulet</h1>
+                <p className="text-gray-600">
+                Pantang menyerah dalam menghadapi masalah.
+                </p>
+
+                <h1 className="text-gray-700 text-xl py-4 font-semibold">Tekun</h1>
+                <p className="text-gray-600">
+                Serius dan teliti dalam menjalani pekerjaan sehari-hari.
+                </p>
+
+                <h1 className="text-gray-700 text-xl py-4 font-semibold">Jujur</h1>
+                <p className="text-gray-600">
+                Selalu menyampaikan informasi sesuai fakta.
+                </p>
+
+                <h1 className="text-gray-700 text-xl py-4 font-semibold">Komunikatif</h1>
+                <p className="text-gray-600">
+                Berkomunikasi dengan ramah, sopan santun, dan bahasa yang baik dan beretika.
+                </p>
+
+                <h1 className="text-gray-700 text-xl py-4 font-semibold">Selalu Mencari Pengetahuan</h1>
+                <p className="text-gray-600 pb-4">
+                Berkomitmen pada pendidikan berkelanjutan dan pembelajaran hal-hal baru.
+                </p>
+
                         
 
                 <Heading title="STRUKTUR PERUSAHAAN"/>
