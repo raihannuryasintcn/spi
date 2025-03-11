b090562b47cf8fa4e96251f521095807a1d69675
adriyansptze <adriyanseptiazi@gmail.com>
2025-03-12 02:15:53 +0700

    last fo the last footer saya lupa wkwkwk

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 export function Footer() {
     return (
-        <footer className="text-gray-200 w-full bg-blue-700 p-4">
-            <h1 className="text-gray-200 text-xl pb-4 font-semibold">OFFICE/WORKSHOP</h1>
+        <footer className="text-gray-200 w-full bg-blue-700 p-6">
+            <h1 className="text-xl font-semibold mb-2">OFFICE / WORKSHOP</h1>
+            <p className="mb-1">
+                Komplek Ruko Melawai Blok B No. 25, Taman Lembah Hijau – Lippo Cikarang
+            </p>
+            <p className="mb-1">
+                <strong>Office I:</strong> Phone: 021-89908451 | Fax: 021-89908451
+            </p>
             <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fuga, dolorem cupiditate praesentium esse, tempora minus at eligendi blanditiis, illo hic ad dignissimos suscipit laboriosam harum optio! Eaque, quibusdam officia!
+                <strong>Office II:</strong> Phone: 021-89903234, 021-89902463 | Fax: 021-89902599
             </p>
         </footer>
     );
