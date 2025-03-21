a8b349907e9469e7563c9134c396f4de95bd54da
raihannuryasintcn <raihannuryasintcn@gmail.com>
2025-03-22 04:45:52 +0700

    optimisasi SEO

--- src/index.jsx
+++ src/index.jsx
@@ -14,6 +14,7 @@ import { Contact } from './pages/Contact.jsx';
 import { Analytics } from "@vercel/analytics/react"
 
 
+
 export function App() {
 	return (
 		<LocationProvider>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
+import sitemap from 'vite-plugin-sitemap';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    sitemap({
+      hostname: 'https://scarletpowerinonesia.com',
+    }),    
+    preact({
+      prerender: { enabled:true }
+    }),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: "PT SCARLET POWER INDONESIA",
+          description: "PT Scarlet Power Indonesia adalah spesialis AC yang menawarkan layanan konsultasi, penjualan, pemasangan, dan perawatan AC untuk rumah dan industri. Dapatkan solusi AC berkualitas, hemat energi, dan tahan lama untuk kenyamanan udara terbaik. meta otomatis",
+        },
+      },
+    }),
+], 
+
   // Additional configuration if needed
 });
