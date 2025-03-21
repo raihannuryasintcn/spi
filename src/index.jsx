10a3f9e323ea6be222acda78843a34e45370e63a
raihannuryasintcn <raihannuryasintcn@gmail.com>
2025-03-22 05:10:32 +0700

    perbaikan layout  dari index ke app.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,48 +1,56 @@
-import { LocationProvider, Router, Route, hydrate, prerender as ssr } from 'preact-iso';
+import {
+  LocationProvider,
+  Router,
+  Route,
+  hydrate,
+  prerender as ssr,
+} from "preact-iso";
 
-import { Header } from './components/Header.jsx';
-import { NotFound } from './pages/_404.jsx';
-import './style.css';
-import { Footer } from './components/Footer.jsx';
-import {  Home } from './pages/Home.jsx';
-import { About } from './pages/About.jsx';
-import { Gallery } from './pages/Gallery.jsx';
-import { Products } from './pages/Products.jsx';
-import { Clients } from './pages/Clients.jsx';
-import { Sewa } from './pages/Sewa.jsx';
-import { Contact } from './pages/Contact.jsx';
-import { Analytics } from "@vercel/analytics/react"
-
-
+import { Header } from "./components/Header.jsx";
+import { NotFound } from "./pages/_404.jsx";
+import "./style.css";
+import { Footer } from "./components/Footer.jsx";
+import { Home } from "./pages/Home.jsx";
+import { About } from "./pages/About.jsx";
+import { Gallery } from "./pages/Gallery.jsx";
+import { Products } from "./pages/Products.jsx";
+import { Clients } from "./pages/Clients.jsx";
+import { Sewa } from "./pages/Sewa.jsx";
+import { Contact } from "./pages/Contact.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 export function App() {
-	return (
-		<LocationProvider>
-			<Header />
-			<main>
-				<Router>
-					<Route path="/" component={Home} />
-					<Route path="/about" component={About} />
-					<Route path="/gallery" component={Gallery} />
-					<Route path="/products" component={Products} />
-					<Route path="/clients" component={Clients} />
-					<Route path="/sewa" component={Sewa} />
-					<Route path="/contact" component={Contact} />
-					<Route path="/legalitas" component={About} />
+  return (
+    <LocationProvider>
+      <div className="bg-[url(/background.png)] bg-contain bg-fixed bg-blend-overlay bg-black/20 font-poppins">
+        <div className="max-w-[1200px] w-full mx-auto py-8 px-16">
+          <Header />
+          <main>
+            <Router>
+              <Route path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/gallery" component={Gallery} />
+              <Route path="/products" component={Products} />
+              <Route path="/clients" component={Clients} />
+              <Route path="/sewa" component={Sewa} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/legalitas" component={About} />
 
-					<Route default component={NotFound} />
-				</Router>
-			</main>
-			<Footer />
-			<Analytics /> 		
-		</LocationProvider>
-	);
+              <Route default component={NotFound} />
+            </Router>
+          </main>
+          <Footer />
+        </div>
+      </div>
+      <Analytics />
+    </LocationProvider>
+  );
 }
 
-	if (typeof window !== 'undefined') {
-		hydrate(<App />, document.getElementById('app'));
-	}
+if (typeof window !== "undefined") {
+  hydrate(<App />, document.getElementById("app"));
+}
 
 export async function prerender(data) {
-	return await ssr(<App {...data} />);
+  return await ssr(<App {...data} />);
 }
